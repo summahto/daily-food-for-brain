da74ccc02016a1d33cbd3b5fc13294a0de8e6738
summahto <sumitkumarmahto3@gmail.com>
2023-11-15 12:19:47 -0500

    unique 3-length palindrome Subsequence completed using 2 pointer strategy

--- DSA/src/com/string/manipulation/UniqueLength3Subseq.java
+++ DSA/src/com/string/manipulation/UniqueLength3Subseq.java
@@ -6,28 +6,39 @@ import java.util.Set;
 public class UniqueLength3Subseq {
 
     public static void main(String[] args) {
+//                  l     r
+        String s = "bbcbaba";
 
-        String s = "aabca";
-        int count = 0, l = 0, r = s.length() -1;
+        int count = 0, l = 0, r = 2;
+        HashSet<Character> first = new HashSet<>();
 
-//        while(l <= r - 2) {
-//
-//            if(s.charAt(l) == s.charAt(r)){
-//
-//                Set<Character> uniq = new HashSet<>();
-//                for(int i = l+1; i< r; i++){
-//
-//                    if(uniq.add(s.charAt(i)))
-//                        count++;
-//                }
-//
-//            }
-//
-//            l++;
-//        }
+        while(l < s.length() - 2) {
+
+            if (first.add(s.charAt(l))){
+
+                Set<Character> uniq = new HashSet<>();
+                int k = l+1;
+                while(r < s.length()){
+
+                    if(s.charAt(l) == s.charAt(r)){
+
+                        for(int i = k; i< r; i++, k++){
+
+                            if(uniq.add(s.charAt(i)))
+                                count++;
+                        }
+                    }
+                    r++;
+                }
+            }
+            l++;
+            r = l+2;
+        }
+
+//        System.out.println();
+
+        System.out.println(count);
 
 
-        System.out.println('a' + 'b');
-        System.out.println(new StringBuilder('a' + 'b'));
     }
 }
